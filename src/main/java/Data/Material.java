4fc6061992ce090472b18f544f96862a406d39e1
unknown <kasper-jeppesen94@hotmail.com>
2019-04-13 08:14:10 +0200

    tilføjede CalculateBOM class, skrev kode til material og Lineitem

--- src/main/java/Data/Material.java
+++ src/main/java/Data/Material.java
@@ -11,4 +11,54 @@ package Data;
  */
 public class Material {
     
+    private int id;
+    private String materialName;
+    private int length;
+    private String unit;
+    private String description;
+    private int price;
+
+    public Material(int id, String materialName, int length, String unit, String description, int price)
+    {
+        this.id = id;
+        this.materialName = materialName;
+        this.length = length;
+        this.unit = unit;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getMaterialName()
+    {
+        return materialName;
+    }
+
+    public int getLength()
+    {
+        return length;
+    }
+
+    public String getUnit()
+    {
+        return unit;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public int getPrice()
+    {
+        return price;
+    }
+    
+    
+    
+    
 }
---
+++ src/main/java/Logic/CalculateBOM.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+import Data.LineItem;
+
+/**
+ *
+ * @author Kasper Jeppesen
+ */
+public class CalculateBOM
+{
+    private LineItem calculateStolper(int length)
+    {
+        
+    }
+}

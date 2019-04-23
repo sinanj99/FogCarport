0f30bf312899a1a7b9f7d0321ac3a4c6620b754e
Sinan Jasar <sinanjasar@Sinans-MacBook-Pro.local>
2019-04-23 12:08:43 +0200

    test

--- src/main/java/Data/IMaterialMapper.java
+++ src/main/java/Data/IMaterialMapper.java
@@ -13,7 +13,7 @@ import java.util.List;
  * @author sinanjasar
  */
 public abstract class IMaterialMapper {
-
+    //teknika
     public static IMaterialMapper instance() {
         return MaterialMapper.getInstance();
     }

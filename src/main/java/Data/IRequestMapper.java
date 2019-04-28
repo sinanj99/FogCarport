39e2cd9a9bbb5d63f8610890e07afe5efe2b6319
Sinan Jasar <sinanjasar@Sinans-MacBook-Pro.local>
2019-04-28 18:58:03 +0200

    changed request mappers

--- src/main/java/Data/IRequestMapper.java
+++ src/main/java/Data/IRequestMapper.java
@@ -24,10 +24,10 @@ public abstract class IRequestMapper {
     public abstract Request getRequest(int id) throws NoSuchRequestException;
     public abstract Carport getRequestCarport(int request_id) throws NoSuchCarportException;
     public abstract Shed getRequestShed(int request_id) throws NoSuchShedException;
-    public abstract void insertRequest(int user_id, int roof_id, boolean inclined, int width, int length, boolean shed, int shedWidth, int shedLength, String datePlaced);
+    public abstract void insertRequest(Request req);
     public abstract void insertRequestCarport(int request_id, int roof_id, boolean inclined, int width, int length, boolean shed);    
     public abstract void insertRequestShed(int request_id, int shedWidth, int shedLength);        
-    public abstract Roof getRoof(int id) throws NoSuchRoofException;
+    public abstract Roof getRoof(String name) throws NoSuchRoofException;
     public abstract List<Roof> getRoofs(int rooftype) throws NoSuchRoofException;
     public abstract void updateRoofPrice(int roof_id, int price) throws NoSuchRoofException;
     public abstract void insertRoof(String name, int price, boolean inclined);

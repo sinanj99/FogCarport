001c9a1bb0f2ce06eabf925f3ad00ace151b4383
Sinan Jasar <sinanjasar@Sinans-MacBook-Pro.local>
2019-05-02 20:02:22 +0200

    changed constructor & fields - a carport now doesnt have boolean shed, and boolean inclined

--- src/main/java/Data/Entity/Carport.java
+++ src/main/java/Data/Entity/Carport.java
@@ -11,34 +11,17 @@ package Data.Entity;
  */
 public class Carport {
     private Roof roof;
-    private boolean inclined;
-    private double inclination;
+    private int inclination;
     private int width;
     private int length;
-    private boolean shed;
     private Shed shed_;
 
-    public Carport(Roof roof, boolean inclined, int width, int length, boolean shed, Shed shed_) {
+    public Carport(Roof roof, int inclination, int width, int length, Shed shed_) {
         this.roof = roof;
-        this.inclined = inclined;
-        this.width = width;
-        this.length = length;
-        this.shed = shed;
-        this.shed_ = shed_;
-    }
-    
-    public Carport(Roof roof, boolean inclined, int width, int length, boolean shed, Shed shed_, double inclination) {
-        this.roof = roof;
-        this.inclined = inclined;
-        this.width = width;
-        this.length = length;
-        this.shed = shed;
-        this.shed_ = shed_;
         this.inclination = inclination;
-    }
-
-    public boolean isInclined() {
-        return inclined;
+        this.width = width;
+        this.length = length;
+        this.shed_ = shed_;
     }
 
     public int getWidth() {
@@ -52,10 +35,6 @@ public class Carport {
         return length;
     }
 
-    public boolean isShed() {
-        return shed;
-    }
-
     public Shed getShed_() {
         return shed_;
     }
@@ -63,12 +42,10 @@ public class Carport {
     public Roof getRoof() {
         return roof;
     }
-
+    
     @Override
     public String toString() {
-        return "Carport{" + "roof=" + roof + ", inclined=" + inclined + ", width=" + width + ", length=" + length + ", shed=" + shed + ", shed_=" + shed_ + '}';
-    }
-    
-    
+        return "Carport{" + "roof=" + roof + ", inclination=" + inclination + ", width=" + width + ", length=" + length + ", shed_=" + shed_ + '}';
+    } 
     
 }

aae20073df94dc56590afc981d3a8f3e51ab6441
Obaydah <obaydahmp@hotmail.com>
2019-05-19 23:02:15 +0200

    Finished validation for carport requests

--- src/main/java/Presentation/Commands/UpdateResponsePriceCommand.java
+++ src/main/java/Presentation/Commands/UpdateResponsePriceCommand.java
@@ -5,12 +5,13 @@
  */
 package Presentation.Commands;
 
-import Presentation.Exceptions.NoSuchMaterialException;
-import Presentation.Exceptions.NoSuchRoofException;
+import Data.Entity.User;
+import Presentation.Exceptions.InvalidInputException;
 import Presentation.Exceptions.UserNotFoundException;
 import com.mysql.cj.util.StringUtils;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.regex.Pattern;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
@@ -21,11 +22,30 @@ import javax.servlet.http.HttpServletRequest;
 public class UpdateResponsePriceCommand implements Command{
 
     @Override
-    public String execute(HttpServletRequest request) throws NoSuchMaterialException, UserNotFoundException, NoSuchRoofException{
-        boolean isn = StringUtils.isStrictlyNumeric(request.getParameter("sellprice"));
-        int id = (int) Integer.parseInt(request.getParameter("requestID"));
-        if(isn) request.setAttribute("updatedPrice", Integer.parseInt(request.getParameter("sellprice")));
+    public String execute(HttpServletRequest request) throws UserNotFoundException, InvalidInputException{
+        User user = (User) request.getSession().getAttribute("user");
+        if(user == null) return "jsp/frontpage.jsp";
+        if(user.isSeller() != true) return "jsp/frontpage.jsp";
         
+        int id = 0;
+        int newPrice = 0;
+        String target = "";
+        
+        try{
+            id = Integer.parseInt(request.getParameter("requestID"));
+        }catch(NumberFormatException e){
+            throw new InvalidInputException("FrontController?command=showrequests", "Indtast venligst kun tal!");
+        }
+        
+        try{
+            newPrice = Integer.parseInt(request.getParameter("sellprice"));
+        }catch(NumberFormatException e){
+            target = "FrontController?command=createresponse&requestID=" + id;
+            throw new InvalidInputException(target, "Indtast venligst kun tal!");
+        }
+        if(newPrice == 0) throw new InvalidInputException("FrontController?command=createresponse&requestID=" + id, "En carport må ikke gives væk gratis!");
+        
+        request.setAttribute("updatedPrice", newPrice);
         return "FrontController?command=createresponse&requestID=" + id;
     }
     

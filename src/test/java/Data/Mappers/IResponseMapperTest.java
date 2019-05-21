b54f5b1896712ef2c29f98653d498120ae5596d1
Obaydah <obaydahmp@hotmail.com>
2019-05-21 14:38:06 +0200

    test

--- src/test/java/Data/Mappers/IResponseMapperTest.java
+++ src/test/java/Data/Mappers/IResponseMapperTest.java
@@ -7,7 +7,10 @@ package Data.Mappers;
 
 import Data.Entity.Response;
 import Presentation.Exceptions.NoSuchResponseException;
+import Presentation.Exceptions.SystemErrorException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.sql.DataSource;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -65,7 +68,12 @@ public class IResponseMapperTest {
         int userId = 0;
         IResponseMapper instance = new IResponseMapperImpl();
         List<Response> expResult = null;
-        List<Response> result = instance.getResponses(userId);
+        List<Response> result = null;
+        try {
+            result = instance.getResponses(userId);
+        } catch (SystemErrorException ex) {
+            Logger.getLogger(IResponseMapperTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
         assertEquals(expResult, result);
         fail("The test case is a prototype.");
     }
@@ -76,7 +84,14 @@ public class IResponseMapperTest {
         int requestId = 0;
         IResponseMapper instance = new IResponseMapperImpl();
         Response expResult = null;
-        Response result = instance.getResponse(requestId);
+        Response result = null;
+        try {
+            result = instance.getResponse(requestId);
+        } catch (NoSuchResponseException ex) {
+            Logger.getLogger(IResponseMapperTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SystemErrorException ex) {
+            Logger.getLogger(IResponseMapperTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
         assertEquals(expResult, result);
         fail("The test case is a prototype.");
     }
@@ -86,7 +101,11 @@ public class IResponseMapperTest {
         System.out.println("insertResponse");
         Response res = null;
         IResponseMapper instance = new IResponseMapperImpl();
-        instance.insertResponse(res);
+        try {
+            instance.insertResponse(res);
+        } catch (SystemErrorException ex) {
+            Logger.getLogger(IResponseMapperTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
         fail("The test case is a prototype.");
     }
 

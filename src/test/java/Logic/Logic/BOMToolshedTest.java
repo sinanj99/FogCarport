4c57172667e695f0c9e79197b91a51f2043c6905
Sinan Jasar <sinanjasar@live.dk>
2019-05-29 19:18:11 +0200

    added shed clothing

---
+++ src/test/java/Logic/Logic/BOMToolshedTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic.Logic;
+
+import Data.Entity.Carport;
+import Data.Entity.Roof;
+import Data.Entity.Shed;
+import org.junit.BeforeClass;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author sinanjasar
+ */
+public class BOMToolshedTest {
+    private static BOMToolshed bom;
+    private static Carport carport;
+    
+    @BeforeClass
+    public static void setUpClass() {
+        bom = new BOMToolshed();
+        carport = new Carport(new Roof(1,"roof",false),0,600,780,new Shed(530, 210));
+    }
+    @Test 
+    public void shedClothingTest() {
+        int expMin = 190;
+        int expMax = 200;
+        int result = bom.calculateQuantityForShedClothing(carport);
+        assertTrue(expMin <= result && result <= expMax);
+    }
+}
